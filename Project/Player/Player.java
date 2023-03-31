54940e44b6e4abce8327a2f8a5fe45cae64af39d
GoldenButterMan <128435596+GoldenButterMan@users.noreply.github.com>
2023-03-31 16:12:27 -0700

    Added javadocs and comments to the player class

--- Project/Player/Player.java
+++ Project/Player/Player.java
@@ -5,7 +5,13 @@ import java.util.Stack;
 import java.util.Random;
 import src.Card;
 import src.Deck;
-
+/**
+ * Class for the player actions.
+ * 
+ * @author Spencer Peterson
+ * Student Number: 230157543
+ *
+ */
 public class Player {
 	private int total;
 	private ArrayList<Card> hand;
@@ -15,45 +21,46 @@ public class Player {
 		total = 10000000;
 		hand = new ArrayList<>();
 	}
-	
+	//How much the player is betting
 	public int bet(int wager) throws IllegalArgumentException{
 		if(wager > total || wager < 0) {
 			throw new IllegalArgumentException("This wager is invalid, please enter a different amount");
 		}
 		else {
+			//Subtract bet from total
 			total -= wager;
 			return wager;
 		}
 	}
-	
+	//Set the player's hand
 	public void setHand(Deck deck) {
 		for(int i = 0; i < 5; i++) {
 			this.hand.add(deck.getShuffledDeck().pop());
 		}
 	}
-	
+	//Get the player's hand
 	public ArrayList<Card> getHand() {
 		return this.hand;
 	}
-	
+	//Get the total a player has.
 	public int getTotal() {
 		return total;
 	}
-	
+	//Set the total the player has
 	public void setTotal(int earn) {
 		total += earn;
 	}
 	
 
-	
+	//Discard a card
 	public void discard(int card) {
 		hand.remove(card);
 	}
-	
+	//Fold player's hand
 	public void fold() {
 		hand.removeAll(hand);
 	}
-	
+	//Draw a card
 	public void drawCard(Card card) {
 		hand.add(card);
 	}
